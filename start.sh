cd04ccd94500bf8b0a5256c1dbf069dfef00250d
sunriseydy <30967445+sunriseydy@users.noreply.github.com>
2018-03-09 14:08:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,10 +22,12 @@ cd /usr/local/
 mv ./hadoop-3.0.0/ ./hadoop
 chown -R hadoop ./hadoop
 cd /usr/local/hadoop
-./bin/hadoop version
+export PATH=$PATH:/usr/local/hadoop/bin
+echo "export PATH=$PATH:/usr/local/hadoop/bin" >> /home/hadoop/.bashrc
 echo "export PDSH_RCMD_TYPE=ssh" >> /home/hadoop/.bashrc
 source /home/hadoop/.bashrc
 export PDSH_RCMD_TYPE=ssh
+hadoop version
 sudo -u hadoop ssh-keygen -t rsa -P '' -f /home/hadoop/.ssh/id_rsa
 sudo -u hadoop cat /home/hadoop/.ssh/id_rsa.pub >> /home/hadoop/.ssh/authorized_keys
 sudo -u hadoop chmod 0600 /home/hadoop/.ssh/authorized_keys
