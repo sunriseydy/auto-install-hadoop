6730c075b8f99984fb6a48f755d39e98289442b8
SunriseYDY <sunriseydy@gmail.com>
2018-03-20 17:23:27 +0800

    Update start-2.7.5.sh

--- start-2.7.5.sh
+++ start-2.7.5.sh
@@ -25,10 +25,10 @@ cd /usr/local/hadoop
 export PATH=$PATH:/usr/local/hadoop/bin
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 export HADOOP_HOME=/usr/local/hadoop
-echo "export PATH=$PATH:/usr/local/hadoop/bin" >> /home/hadoop/.bashrc
+echo "PATH=$PATH:/usr/local/hadoop/bin" >> /home/hadoop/.bashrc
 echo "HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar" >> /home/hadoop/.bashrc
-echo "export HADOOP_HOME=/usr/local/hadoop" >> /home/hadoop/.bashrc
-echo "export PDSH_RCMD_TYPE=ssh" >> /home/hadoop/.bashrc
+echo "HADOOP_HOME=/usr/local/hadoop" >> /home/hadoop/.bashrc
+echo "PDSH_RCMD_TYPE=ssh" >> /home/hadoop/.bashrc
 source /home/hadoop/.bashrc
 export PDSH_RCMD_TYPE=ssh
 hadoop version
