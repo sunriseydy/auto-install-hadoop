88a70ca8cdaa0d1776a3a47e005a8c76ba031843
SunriseYDY <sunriseydy@gmail.com>
2018-04-03 00:08:03 +0800

    Update start-2.7.5.sh

--- start-2.7.5.sh
+++ start-2.7.5.sh
@@ -8,27 +8,34 @@ deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe 
 apt-get update
 apt-get upgrade -y
 apt-get install -y vim openssh-server openjdk-8-jdk wget tar pdsh rsync
-echo 'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"' >> /etc/environment
+echo 'export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"' >> /etc/profile
+echo 'export CLASSPATH=.:$JAVA_HOME/lib' >> /etc/profile
+export CLASSPATH=.:$JAVA_HOME/lib
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-source /etc/environment
+source /etc/profile
 echo "create hadoop user,the password is hadoop"
 useradd -m hadoop -s /bin/bash
 echo hadoop:hadoop|chpasswd
 adduser hadoop sudo
 adduser hadoop root
+echo 'export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"' >> /home/hadoop/.bashrc
+echo 'export CLASSPATH=.:$JAVA_HOME/lib' >> /home/hadoop/.bashrc
+source /home/hadoop/.bashrc
 wget -O hadoop-2.7.5.tar.gz https://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.7.5/hadoop-2.7.5.tar.gz
 tar -zxf hadoop-2.7.5.tar.gz -C /usr/local
 cd /usr/local/
 mv ./hadoop-2.7.5/ ./hadoop
 chown -R hadoop:hadoop ./hadoop
 cd /usr/local/hadoop
-export PATH=$PATH:/usr/local/hadoop/bin
-export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
+export PATH=$PATH:/usr/local/hadoop/bin:/usr/local/hadoop/sbin
 export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$CLASSPATH:`$HADOOP_HOME/bin/hadoop classpath`
+export PATH=${JAVA_HOME}/bin:${PATH}
 echo "PATH=$PATH:/usr/local/hadoop/bin" >> /home/hadoop/.bashrc
-echo "HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar" >> /home/hadoop/.bashrc
 echo "HADOOP_HOME=/usr/local/hadoop" >> /home/hadoop/.bashrc
-echo "PDSH_RCMD_TYPE=ssh" >> /home/hadoop/.bashrc
+echo 'export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$CLASSPATH:`$HADOOP_HOME/bin/hadoop classpath`' >> /home/hadoop/.bashrc
+echo 'export CLASSPATH=$CLASSPATH:$HADOOP_CLASSPATH' >> /home/hadoop/.bashrc
+echo "export PDSH_RCMD_TYPE=ssh" >> /home/hadoop/.bashrc
 source /home/hadoop/.bashrc
 export PDSH_RCMD_TYPE=ssh
 hadoop version
